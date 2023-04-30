66a5f5746a7c5c217d64fd3676cbada4f8b01b26
root <root@ser950003025975.local>
2023-04-30 13:29:26 +0000

    start list.h

---
+++ include/list.h
@@ -0,0 +1,14 @@
+/**
+ * @file list.h
+ * @author Zhuir Xiao (xzr3356142450@gmail.com)
+ * @brief `list.h`提供链表数据结构的一些基本操作. 
+ * @version 0.1
+ * @date 2023-04-30
+ * 
+ * @todo 
+ *  1. 需要进行测试, 以测试函数正确性
+ * 
+ * @warning 
+ * 
+ * @copyright Copyright Zhuiri Xiao (c) 2023 with GNU Public License V3.0
+ */
