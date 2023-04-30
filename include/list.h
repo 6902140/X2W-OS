0d93c079452e0b696b0bd2d33e46971c29e3cb8a
root <root@ser950003025975.local>
2023-04-30 13:30:46 +0000

    finish init_list

--- include/list.h
+++ include/list.h
@@ -12,3 +12,32 @@
  * 
  * @copyright Copyright Zhuiri Xiao (c) 2023 with GNU Public License V3.0
  */
+
+
+#include "types.h"
+
+struct list_head {
+	struct list_head *next, *prev;
+};
+
+#define POISON_POINTER_DELTA 0xdead000000000000
+#define LIST_POISON1  ((void *) 0x00100100 + POISON_POINTER_DELTA)
+#define LIST_POISON2  ((void *) 0x00200200 + POISON_POINTER_DELTA)
+
+#define LIST_HEAD_INIT(name) {&(name), &(name)}
+
+#define LIST_HEAD(name) \
+	struct list_head name = LIST_HEAD_INIT(name)
+
+
+/**
+ * @brief 'INIT_LIST_HEAD' 初始化一个list的头指针
+ * 
+ * @return 返回一个用户物理页的起始地址
+ * 
+ */
+static inline void INIT_LIST_HEAD(struct list_head *list)
+{
+	list->next = list;
+	list->prev = list;
+}
